43584574acb06aabe446e178f3b3935788eb6a38
SimonAlphonse <91269886+SimonAlphonse@users.noreply.github.com>
2023-03-14 17:59:11 +0530

    Added Azure Function Sample

---
+++ Function/Function/Function.cs
@@ -0,0 +1,32 @@
+using System.Net;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace Function
+{
+    public class Function
+    {
+        private readonly ILogger _logger;
+
+        public Function(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<Function>();
+        }
+
+        [Function("Function")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+
+            //response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
